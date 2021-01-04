a55569a0dc1488569f7b1bcb1f9a9a20a44c628e
dikiamust <agungmustaqimdiki@gmail.com>
2021-01-04 11:37:58 +0700

    Merge remote-tracking branch 'origin/main' into main

--- instagram/part_1/extraction.py
+++ instagram/part_1/extraction.py
@@ -1,15 +1,20 @@
 #SCRAPING ACCOUNT (USER NAME, FULL NAME & PROFILE PICT) THAT LIKED ON AN INSTGRAM POST
 #SCRAPING AKUN (USER NAME, FULL NAME & PROFILE PICT) YANG MENYUKAI DI POSTINGAN INSTGRAM
 
-import requests, json, time
+import requests, json, time, csv
 
 url ='https://www.instagram.com/graphql/query'
 
 short_code = input(' Please input a short code: ')
 
 end_cursor = ''
-
 count = 0
+counter_file = 1
+jumlah_per_file = 10000
+
+writer = csv.writer(open('directory_hasil_like/{}{}.csv'.format(short_code, counter_file), 'w', newline=''))
+headers = ['User Name', 'Full Name', 'Profile Pic']
+writer.writerow(headers)
 while 1 :
     varibles = {
         "shortcode": short_code,
@@ -28,15 +33,24 @@ while 1 :
     try: users = r['data']['shortcode_media']['edge_liked_by']['edges']
     except:
         print('Wait for 10 secs')
-        time.sleep(30)
+        time.sleep(10)
         continue
 
     for user in users:
+        if count % jumlah_per_file == 0 and count!=0 :
+            counter_file +=1
+            writer = csv.writer( open( 'directory_hasil_like/{}{}.csv'.format( short_code, counter_file ), 'w', newline='' ) )
+            headers = ['User Name', 'Full Name', 'Profile Pic']
+            writer.writerow( headers )
+
         username = user['node']['username']
         full_name = user['node']['full_name']
         profile_pic = user['node']['profile_pic_url']
         count += 1
         print(count, username, full_name, profile_pic)
+        writer = csv.writer( open( 'directory_hasil_like/{}{}.csv'.format( short_code, counter_file ), 'a', newline='', encoding='utf-8' ) )
+        data = [username, full_name, profile_pic]
+        writer.writerow( data )
 
 
     end_cursor = r['data']['shortcode_media']['edge_liked_by']['page_info']['end_cursor']
